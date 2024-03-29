2872a9c0105724944ba40ecfec1e89bb6870c130
diazAngelFR <165363474+diazAngelFR@users.noreply.github.com>
2024-03-29 01:10:11 -0600

    Update and rename project.v to tt_um_ALU.v

--- src/tt_um_ALU.v
+++ src/tt_um_ALU.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_ALU (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,11 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    ALU C1 (.A(ui_in[3:0]), .B(ui_in[7:4]), .SEL(uio_in[2:0]), .ALU_OUT(uo_out[3:0]), .CARRY_OUT(uo_out[4]), .OVERFLOW(uo_out[5]), .ZERO(uo_out[6]), .SIGNO(uo_out[7]));
 
+    assign uio_oe = 8'b00000000;
+    assign uio_out [7:0] = 8'b00000000;
+
+
+    
 endmodule
